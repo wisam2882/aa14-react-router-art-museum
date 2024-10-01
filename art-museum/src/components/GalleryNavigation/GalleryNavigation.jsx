0a22a6e934cdc1f2a50d7d243b765810b52736c3
ZiwenZhang <a60595677@gmail.com>
2024-10-01 17:52:47 -0400

    finished phase2

--- art-museum/src/components/GalleryNavigation/GalleryNavigation.jsx
+++ art-museum/src/components/GalleryNavigation/GalleryNavigation.jsx
@@ -1,9 +1,22 @@
-
+import { NavLink } from "react-router-dom"
+// import React from "react"
 const GalleryNavigation = ({galleries}) => {
-    console.log(galleries)
+    // console.log(galleries)
     return (
         <nav>
             <h1>Galleries</h1>
+            <NavLink to="/">Home</NavLink>
+            <ul>
+            {
+                galleries.map((gallery) =>(
+                    <li>
+                    <NavLink key={gallery.id} to={`/galleries/${gallery.id}`}>
+                        {gallery.name}
+                    </NavLink>
+                    </li>
+                ))
+            }
+            </ul>
         </nav>
     )
 }
